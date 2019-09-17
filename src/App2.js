8a174aac6d16872dfa94b901f836ef98b71f92bc
amusameh <abdalsamad.y.m@gmail.com>
2019-09-17 15:16:28 +0300

    changes

---
+++ src/App2.js
@@ -0,0 +1,109 @@
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+import dataJson from './data.json';
+// import createData from './createData';
+
+class App extends Component {
+  state = {
+    data: dataJson,
+    chartWidth: 800,
+    chartHeight: 800,
+  };
+
+  componentDidMount() {
+    const { chartWidth, chartHeight, data } = this.state;
+
+    const svg = d3
+      .select('#chart')
+      .append('svg')
+      .attr('width', chartWidth)
+      .attr('height', chartHeight);
+
+    const minRate = d3.min(data.map(d => d.diffRate));
+    const maxRate = d3.max(data.map(d => d.diffRate));
+
+    const quantizeScale = d3
+      .scaleQuantize()
+      .domain([minRate, maxRate])
+      .range(['lightblue', 'orange', 'lightgreen', 'pink']);
+
+    const nodes = svg
+      .selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('class', 'node bubble')
+      .attr('fill', d => {
+        return quantizeScale(d.diffRate);
+      })
+      .attr('stroke-width', 2)
+      .attr('r', d => d.raduis / 20)
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y);
+
+    this.createSimulation();
+  }
+
+  createSimulation = () => {
+    const { data, chartHeight } = this.state;
+    const minRate = d3.min(data.map(d => d.diffRate));
+    const maxRate = d3.max(data.map(d => d.diffRate));
+
+    const yAxiesScale = d3
+      .scaleQuantize()
+      .domain([minRate, maxRate])
+      .range([0, 0.25, 0.5, 1]);
+
+    const forceX = d3.forceX().x(d => 400);
+    // .strength(1);
+
+    // const forceY = d3.forceY().y(d => yAxiesScale(d.diffRate) * chartHeight);
+    const forceY = d3.forceY().y(d => {
+      let x = 0;
+      if (d.diffRate < 0) {
+        x = 300;
+      } else if (d.diffRate < 100) {
+        x = 250;
+      } else if (d.diffRate < 200) {
+        x = 100;
+      } else {
+        x = 50;
+      }
+      return x;
+    });
+    // .strength(1);
+
+    d3.forceSimulation(data)
+      .force(
+        'collide',
+        d3
+          .forceCollide()
+          .radius(d => d.raduis / 20 + 5)
+          .strength(1)
+      )
+      .force('x', forceX)
+      .force('y', forceY)
+      .alpha(0.1) // small alpha to have the elements move at a slower pace
+      .on('tick', () => {
+        // call the tick function running the simulation
+        d3.selectAll('.bubble').attr(
+          'transform',
+          d => `translate(${d.x} ${d.y + 300})`
+        );
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="chart" id="chart">
+          Hello
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
